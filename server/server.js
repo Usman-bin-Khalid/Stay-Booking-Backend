7cccc117ed466d1993d0faa2da43ea47e403ad71
Usman <usmanbinkhalidpk@gmail.com>
2026-01-26 03:41:26 -0800

    Auth Routes Created

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ app.get('/', (req , res) => {
     res.send('API is running...');
 });
 
+app.use('/api/auth', authRoutes);
+
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 })
