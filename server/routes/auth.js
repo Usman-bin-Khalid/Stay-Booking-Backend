e575356cef3b264ccbd6093ff9f210c47fa8aa65
Usman <usmanbinkhalidpk@gmail.com>
2026-01-26 03:55:49 -0800

    User Router Created

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,3 +3,19 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const Profile = require('../models/Profile');
+
+
+router.post('/register', async (req, res) => {
+    const {name, email, password} = req.body;
+    try {
+     const salt = await bcrypt.genSalt(10);
+     const hashedPassword = await bcrypt.hash(password, salt);
+     const user = new User({
+        name, email, password : hashedPassword
+     });
+      await user.save();
+    } catch (err) {
+
+    }
+})
