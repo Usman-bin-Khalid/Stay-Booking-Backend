0da4ce30ae69d8f246cf97aa669f32496b023bdd
Usman <usmanbinkhalidpk@gmail.com>
2026-01-27 01:55:12 -0800

    Register User Route Completed

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -26,8 +26,10 @@ router.post('/register', async (req, res) => {
 
       });
       await profile.save();
-      
+      const token = jwt.sign({id : user._id}, process.env.JWT_SECRET, {expiresIn : '1d'});
+      res.status(201).json({token, user});
     } catch (err) {
-
+     console.error(err);
+     res.status(500).json({message : "Server Error"});
     }
 })
