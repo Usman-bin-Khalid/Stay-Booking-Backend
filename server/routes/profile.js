e787d19a5fcd507f5431f3c8b81e1c40fef5d4a8
Usman <usmanbinkhalidpk@gmail.com>
2026-01-29 22:12:48 -0800

    Profile Get Route Completed

---
+++ server/routes/profile.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const Profile = require('../models/Profile');
+const auth = require('../middleware/auth');
+
+
+router.get('/' , auth, async (req, res) => {
+    try { 
+     const profile = await Profile.findOne({user : req.user.id});
+     if(!profile) return res.status(404).json({message : 'Profile not found'});
+     resjson(profile);
+    } catch (err) {
+        res.status(500).json({message : 'Server Error', err});
+    }
+})
