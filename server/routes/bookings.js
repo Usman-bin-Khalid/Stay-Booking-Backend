787db747e7e965c6e84a982d27bd901cdc9454ec
Usman <usmanbinkhalidpk@gmail.com>
2026-02-06 10:20:49 -0800

    Get Single Booking by Id API Completed

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -21,15 +21,27 @@ router.post('/', auth, async (req, res) => {
 })
 
 
-
+// Get All Bookings
 router.get('/my-bookings' , auth, async (req, res) => {
     try  {
      const bookings = await Booking.find({userId : req.user.id}).populate("listingId");
      res.json(bookings);
 
     } catch (err) {
-
+      res.status(500).json({message : "Error Fetching Bookings"});
     }
+})
+
+// Get a Single booking by there id
+router.get('/:id' , auth, async (req, res) => {
+ try {
+   const booking = await Booking.findById(req.params.id).populate("listingId");
+   if(!booking) return res.status(404).json({message : "Booking not found"});
+   if(booking.userId.toString() !== req.user.id) return res.status(403).json({message : "Unauthorized"});
+   res.json(booking); 
+ } catch (err) {
+   res.status(500).json({message : "Server Error"});
+ }
 })
 
 module.exports = router;
